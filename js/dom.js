7cb585cc5ad2269f05602441fcb1dbaacc25fa40
Beth1998 <tatyliz1998@gmail.com>
2024-10-23 11:52:00 -0500

    jsDom

---
+++ js/dom.js
@@ -0,0 +1,17 @@
+const $head = document.querySelector("#head"); //id
+const $cuerpo = document.querySelector("body"); //etiqueta
+const $footer = document.querySelector(".footer"); //clase
+const $a = document.querySelector("#aitem");
+const $a2 = document.querySelector("#aitem2");
+const $header = document.querySelector(".header");
+const $header2 = document.querySelector(".aheader");
+const $footeritem2 = document.querySelector("#footeritem2")
+
+//Hace referencia a que voy a manipular un objeto del dom
+//Manipulacion de dom
+$a.classList.add("a")
+$a2.classList.add("a2")
+$footer.classList.add("colorFuente")
+$header.classList.add("header")
+$header2.classList.add("aheader")
+$footeritem2.classList.add("footeritem2")
