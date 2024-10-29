c7d3488557f0588de4fc0e885d9439bb7f7e5768
Beth1998 <tatyliz1998@gmail.com>
2024-10-29 12:19:04 -0500

    fragmento

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 
-    console.log(document);
+console.log(document)
 
-    const $flexItem2 = document.querySelector(".flex-item");
-    const $flexItem1 = document.getElementById("flex-item2");
-    const $a1 = document.querySelector("#a1");
-    const $body = document.querySelector("#cuerpo")
+const $flexItem2 = document.querySelector(".flex-item");
+const $flexItem1 = document.getElementById("flex-item2");
+const $a1 = document.querySelector("#a1");
+const $body = document.querySelector("#cuerpo")
 
 
-    const $enviar = document.querySelector("#enviar");
 
-    function cambiarNombre() {
+const $enviar = document.querySelector("#enviar");
+
+function cambiarNombre() {
         let edad = 1;
-        if(edad == 0){
-            $a1.textContent = "facebook"
-        }else{
-            $a1.textContent = "tiktok"
-        }
-    }
-    
-    function cambioColor() {
-        $a1.style.backgroundColor = "yellow";
-        $a1.classList.add("scale");
-        $flexItem2.classList.toggle("rotate");
-        $body.classList.toggle("body");
-    }
 
-    //$enviar.addEventListener("click", cambiarNombre);
+    if (edad == 0) {
+        $a1.textContent = "facebook"
+    }else{
+        $a1.textContent = "tiktok"
+    }
+}
+function cambioColor() {
+    $a1.style.backgroundColor = "yellow";
+    $a1.classList.add("scale");
+    $flexItem2.classList.toggle("rotate");
+    $body.classList.toggle("body");
+}
+//$enviar.addEventListener("click", cambiarNombre)
 
-    $enviar.addEventListener("click", cambioColor);
+$enviar.addEventListener("click", cambioColor)
+
+
+
